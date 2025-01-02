ca4f49d17dc889bfbeebc836a729f9723369c18a
Pramesh Anuradha <anurada58@gmail.com>
2025-01-02 14:54:03 -0500

    Add files via upload

---
+++ js/rashmiJavaScript.js
@@ -0,0 +1,10 @@
+
+	
+	        /* Function to use lastModified property */ 
+        function lastModified() { 
+            var x = document.lastModified; 
+            document.getElementById("sudo").innerHTML = "Last Modified : " +x; 
+			
+        }; 
+	
+
